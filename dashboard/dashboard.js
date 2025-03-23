a6dceb75a3c05a04c9f886adaa939ed3485b253f
Stengaming <xxxnoaemma.nl@gmail.com>
2025-03-23 19:49:40 +0100

    Update dashboard.js

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,6 +1,6 @@
 function checkLogin() {
     if (!sessionStorage.getItem("loggedIn")) {
-        window.location.href = "login/";
+        window.location.href = "index.html";
     }
 }
 
@@ -10,5 +10,5 @@ function loadPage(page) {
 
 function logout() {
     sessionStorage.removeItem("loggedIn");
-    window.location.href = "login/";
+    window.location.href = "index.html";
 }
