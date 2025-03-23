1e4e284dd8c8d7d39150ea645c8b7525e74d48af
Stengaming <xxxnoaemma.nl@gmail.com>
2025-03-23 19:42:11 +0100

    Create dashboard.js

---
+++ dashboard/dashboard.js
@@ -0,0 +1,14 @@
+function checkLogin() {
+    if (!sessionStorage.getItem("loggedIn")) {
+        window.location.href = "login";
+    }
+}
+
+function loadPage(page) {
+    document.getElementById("page-frame").src = page;
+}
+
+function logout() {
+    sessionStorage.removeItem("loggedIn");
+    window.location.href = "login";
+}
